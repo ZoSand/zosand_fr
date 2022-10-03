9d92caec01fc76d0c3f2b909a53dcd4d5531a173
ZoSand <64746594+ZoSand@users.noreply.github.com>
2022-10-03 12:14:38 +0200

    navbar selected background color

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <nav>
     <ul>
-        <li><a href="./index.php">Home</a></li>
+        <li><a href="./index.php" class="active">Home</a></li>
         <li><a href="./contact.php">Contact</a></li>
     </ul>
 </nav>
