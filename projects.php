86b5189ff60ed5965ae3d3c30f07cbf278f73f41
ZoSand <64746594+ZoSand@users.noreply.github.com>
2022-10-03 17:31:09 +0200

    projects placeholders

--- projects.php
+++ projects.php
@@ -1,12 +1,47 @@
 <?php
 include("./header.php");
 ?>
-    <!--TODO: put main body here -->
 
-<?php
-$dbConfig = parse_ini_file('../db.ini');
-echo $dbConfig["username"]
-?>
+    <!--TODO: put main body here -->
+    <div class="timeline">
+        <?php
+        $dbConfig = parse_ini_file('../db.ini');
+        $con = mysqli_connect("localhost", $dbConfig['username'], $dbConfig['password'], $dbConfig['database']);
+        if (!$con) {
+            die("Failed to connect to Database");
+        }
+        ?>
+        <article>
+            <h2>Match contre Joueur A</h2>
+            <ul>
+                <li><span>Lieu</span> Paris</li>
+                <li><span>Date</span> 01/04/2022, </li>
+                <li><span>Places</span> 340</li>
+                <li><span>Billeterie</span> <a href="#">Lien</a></li>
+            </ul>
+        </article>
+        <article>
+            <ul>
+                <li><span>Lieu</span> Paris</li>
+                <li><span>Date</span> 01/04/2022, </li>
+                <li><span>Places</span> 340</li>
+                <li><span>Billeterie</span> <a href="#">Lien</a></li>
+            </ul>
+        </article>
+        <article>
+            <h2>Match contre Joueur A</h2>
+            <ul>
+                <li><span>Lieu</span> Paris</li>
+                <li><span>Date</span> 01/04/2022, </li>
+                <li><span>Places</span> 340</li>
+                <li><span>Billeterie</span> <a href="#">Lien</a></li>
+            </ul>
+        </article>
+        </article>
+        <article>
+            <h2>Match contre Joueur A</h2>
+        </article>
+    </div>
 
 
 <?php
