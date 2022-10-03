5df3286ce7ccab4521f06c2dea47a879471efb57
ZoSand <64746594+ZoSand@users.noreply.github.com>
2022-10-03 12:25:22 +0200

    navbar format

--- navbar.php
+++ navbar.php
@@ -1,13 +1,28 @@
 <nav>
     <ul>
         <li>
-            <a href="/" <?php if($_SERVER['SCRIPT_NAME']=="/index.php") { ?>  class="active"   <?php   }  ?>>Home</a>
+            <a href="/"
+                <?php if ($_SERVER['SCRIPT_NAME'] == "/index.php") { ?>
+                    class="active"
+                <?php } ?>>
+                Home
+            </a>
         </li>
         <li>
-            <a href="/contact.php" <?php if($_SERVER['SCRIPT_NAME']=="/contact.php") { ?>  class="active"   <?php }?>>Contact</a>
+            <a href="/contact.php"
+                <?php if ($_SERVER['SCRIPT_NAME'] == "/contact.php") { ?>
+                    class="active"
+                <?php } ?>>
+                Contact
+            </a>
         </li>
         <li>
-            <a href="/about.php" <?php if($_SERVER['SCRIPT_NAME']=="/about.php") { ?>  class="active"   <?php }?>>About me</a>
+            <a href="/about.php"
+                <?php if ($_SERVER['SCRIPT_NAME'] == "/about.php") { ?>
+                    class="active"
+                <?php } ?>>
+                About me
+            </a>
         </li>
     </ul>
 </nav>
