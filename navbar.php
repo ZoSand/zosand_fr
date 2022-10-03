ad6602881ef3368bd98c51dc0ba7cdba31c64d62
ZoSand <64746594+ZoSand@users.noreply.github.com>
2022-10-03 12:11:36 +0200

    navbar wip

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <nav>
     <ul>
         <li><a href="./index.php">Home</a></li>
-        <!--<li><a href="./contact.php">Contact</a></li>-->
+        <li><a href="./contact.php">Contact</a></li>
     </ul>
 </nav>
