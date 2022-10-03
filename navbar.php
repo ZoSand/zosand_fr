85814564081e9f3afdcc851f3fb1c3669f0822e0
ZoSand <64746594+ZoSand@users.noreply.github.com>
2022-10-03 12:16:35 +0200

    rewrite rule hide php extension

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <nav>
     <ul>
         <li><a href="/" class="active">Home</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><a href="/contact.php">Contact</a></li>
     </ul>
 </nav>
