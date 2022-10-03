d9541b8ea61693053108bf5083c429c480e64979
ZoSand <64746594+ZoSand@users.noreply.github.com>
2022-10-03 15:05:57 +0200

    navbar as an array of URLs

--- navbar.php
+++ navbar.php
@@ -1,39 +1,28 @@
+<?php
+$menu = array(
+    'About' => array('text' => 'About me', 'url' => 'index.php'),
+    'gallery' => array('text' => 'Projects', 'url' => 'unknown.php'),
+    'contact' => array('text' => 'Contact', 'url' => 'contact.php'),
+);
+?>
+
 <header>
     <nav class="navbar">
-        <ul>
-            <li>
-                <a href="/"
-                    <?php if ($_SERVER['SCRIPT_NAME'] == "/index.php") { ?>
-                        class="active"
-                    <?php } ?>>
-                    About Me
-                </a>
-            </li>
-            <?php
-            $pages = scandir("/pages");
-            foreach ($pages as $page) {
-                ?>
-                <li>
-                    <a href="/"
-                        <?php if ($_SERVER['SCRIPT_NAME'] == "/unknown.php") { ?>
-                            class="active"
-                        <?php } ?>>
-                        Projects
-                    </a>
-                </li>
-                <?php
-            }
-            ?>
 
-            <!-- TODO: add sections here -->
-            <li>
-                <a href="/pages/contact.php"
-                    <?php if ($_SERVER['SCRIPT_NAME'] == "/pages/contact.php") { ?>
-                        class="active"
-                    <?php } ?>>
-                    Contact
-                </a>
-            </li>
-        </ul>
+        <?php
+        $html = "<ul>";
+        foreach ($menu as $menuItem) {
+            $html .= "<li>";
+            $html .= "<a href='{$menuItem['url']}'";
+            if ($_SERVER['SCRIPT_NAME'] == "/index.php") {
+                $html .= " class='active'";
+            }
+            $html .= ">";
+            $html .= "{$menuItem['text']}";
+            $html .= "</a>";
+            $html .= "</li>";
+        }
+        $html .= "</ul>";
+        ?>
     </nav>
 </header>
