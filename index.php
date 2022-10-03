860bf72fb7d7eb8e566c96df5182042476f0a813
ZoSand <64746594+ZoSand@users.noreply.github.com>
2022-10-03 11:47:21 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome - ZoSand</title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+    <?php include("./navbar.php") ?>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,6 @@
+<navbar>
+    <ul>
+        <li><a href="./index.php">Home</a></li>
+        <li><a href="./contact.php">Contact</a></li>
+    </ul>
+</navbar>
