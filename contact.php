ec7619d626410dfb8e3f32675d6bdfa45b70ba64
ZoSand <64746594+ZoSand@users.noreply.github.com>
2022-10-03 12:12:20 +0200

    contact added navbar

--- contact.php
+++ contact.php
@@ -1,1 +1,11 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact - ZoSand</title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<body>
+<?php include("./navbar.php") ?>
+</body>
+</html>
