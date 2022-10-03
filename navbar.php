f54e5997314c36504198a75417114d6b38d546fc
ZoSand <64746594+ZoSand@users.noreply.github.com>
2022-10-03 12:27:21 +0200

    navbar format

--- navbar.php
+++ navbar.php
@@ -1,28 +1,30 @@
-<nav>
-    <ul>
-        <li>
-            <a href="/"
-                <?php if ($_SERVER['SCRIPT_NAME'] == "/index.php") { ?>
-                    class="active"
-                <?php } ?>>
-                Home
-            </a>
-        </li>
-        <li>
-            <a href="/contact.php"
-                <?php if ($_SERVER['SCRIPT_NAME'] == "/contact.php") { ?>
-                    class="active"
-                <?php } ?>>
-                Contact
-            </a>
-        </li>
-        <li>
-            <a href="/about.php"
-                <?php if ($_SERVER['SCRIPT_NAME'] == "/about.php") { ?>
-                    class="active"
-                <?php } ?>>
-                About me
-            </a>
-        </li>
-    </ul>
-</nav>
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="/"
+                    <?php if ($_SERVER['SCRIPT_NAME'] == "/index.php") { ?>
+                        class="active"
+                    <?php } ?>>
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="/contact.php"
+                    <?php if ($_SERVER['SCRIPT_NAME'] == "/contact.php") { ?>
+                        class="active"
+                    <?php } ?>>
+                    Contact
+                </a>
+            </li>
+            <li>
+                <a href="/about.php"
+                    <?php if ($_SERVER['SCRIPT_NAME'] == "/about.php") { ?>
+                        class="active"
+                    <?php } ?>>
+                    About me
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
