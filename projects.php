1c7fdfa3cde9a38a55365665942e603e8a1f1f37
ZoSand <64746594+ZoSand@users.noreply.github.com>
2022-10-06 14:41:37 +0200

    projects timeline almost finished

--- projects.php
+++ projects.php
@@ -3,35 +3,40 @@ include("./header.php");
 ?>
 
     <!--TODO: put main body here -->
-    <div class="timeline">
-        <?php
-        $dbConfig = parse_ini_file('../db.ini');
-        try {
-            $db = new PDO(
-                "mysql:host={$dbConfig['host']};dbname={$dbConfig['username']};charset=utf8",
-                $dbConfig["username"],
-                $dbConfig["password"],
-                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-        } catch (Exception $e) {
-            die('Error : ' . $e->getMessage());
-        }
+<?php
+$dbConfig = parse_ini_file('../db.ini');
+try {
+    $db = new PDO(
+        "mysql:host={$dbConfig['host']};dbname={$dbConfig['username']};charset=utf8",
+        $dbConfig["username"],
+        $dbConfig["password"],
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+} catch (Exception $e) {
+    die('Error : ' . $e->getMessage());
+}
 
-        $projectsStatement = $db->prepare('SELECT * FROM projects ORDER BY date');
-        $projectsStatement->execute();
-        $projects = $projectsStatement->fetchAll();
-        foreach ($projects as $project) {
-            ?>
-            <article>
-                <h2><?php echo $project["name"] ?> <small>(<?php echo $project["date"] ?>)</small></h2>
-                <p>
-                    <?php echo $project["description"] ?>
-                </p>
-            </article>
-            <?php
-        }
-        ?>
-    </div>
+$projectsStatement = $db->prepare('SELECT * FROM projects ORDER BY date DESC');
+$projectsStatement->execute() or die(print_r($db->errorInfo(), true));
+$projects = $projectsStatement->fetchAll();
 
+if (count($projects) > 0) {
+    echo '<div class="timeline">';
+}
+
+foreach ($projects as $project) {
+    ?>
+    <article class=project>
+        <h2 class="title" before-date=<?php echo $project["date"] ?>><?php echo $project["name"] ?></h2>
+        <p>
+            <?php echo $project["description"] ?>
+        </p>
+    </article>
+    <?php
+}
+if (count($projects) > 0) {
+    echo '</div>';
+}
+?>
 
 <?php
 include("./footer.php");
