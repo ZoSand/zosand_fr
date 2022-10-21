8e00d4f77957df328350ddc128c7b88dc98a59a3
ZoSand <64746594+ZoSand@users.noreply.github.com>
2022-10-21 15:27:25 +0200

    Homepage

--- index.php
+++ index.php
@@ -2,6 +2,39 @@
 include("./header.php");
 ?>
 <!--TODO: put main body here -->
+<div class="about">
+    <img src="https://cdn.discordapp.com/avatars/295295953919934466/7c96d39f05a947dcdb69f90bf720bc08.webp"
+         alt="Photo">
+
+    <div>
+        <p>Third year college student in Creajeux, I'm interested in everything related directly or indirectly to
+            programming.</p>
+
+        <p> As an expatriate child, I grew up in various African countries, where I discovered video games and my
+            passion
+            for computers and programming in general.</p>
+    </div>
+
+    <div>
+        <h1>Skills</h1>
+        <div class="wrapper">
+            <div class="align-left">
+                <p>Lua</p>
+                <p>C</p>
+                <p>C#</p>
+                <p>C++</p>
+                <p>SFML/CSFML</p>
+            </div>
+            <div class="align-right">
+                <p>Löve2D</p>
+                <p>Unity</p>
+                <p>Unreal Engine#</p>
+                <p>Fmod</p>
+                <p>WWISE</p>
+            </div>
+        </div>
+    </div>
+</div>
 <?php
 include("./footer.php");
 ?>
