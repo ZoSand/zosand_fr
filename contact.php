c2f6c76a43d0600984337111bf9f5fa51b596923
ZoSand <64746594+ZoSand@users.noreply.github.com>
2022-10-21 13:02:04 +0200

    contact form

--- contact.php
+++ contact.php
@@ -1,13 +1,16 @@
 <?php
 if (isset($_POST['username']) && isset($_POST['useremail']) && isset($_POST['usersubject']) && isset($_POST['usermessage']))
 {
-    error_reporting(E_ALL);
     $from = "noreply@zosand.fr";
     $to = "webmaster@zosand.fr";
     $subject = $_POST['usersubject'];
     $message = "Message from: " . $_POST['useremail']. " (" . $_POST['username'] . ") " . $_POST['usermessage'];
     $headers = "From:" . $from;
     mail($to, $subject, $message, $headers);
+    $to = $_POST['useremail'];
+    $message = "You send a message to ". $to . ": " . $_POST['usermessage'];
+    mail($to, $subject, $message, $headers);
+
 }
 
 include("header.php");
