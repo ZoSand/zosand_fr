68fbc918e9a2f165b7e94f9c8ec8fd89618e9ef6
ZoSand <64746594+ZoSand@users.noreply.github.com>
2022-10-21 16:09:07 +0200

    cont

--- contact.php
+++ contact.php
@@ -1,14 +1,13 @@
 <?php
-if (isset($_POST['username']) && isset($_POST['useremail']) && isset($_POST['usersubject']) && isset($_POST['usermessage']))
-{
+if (isset($_POST['username']) && isset($_POST['useremail']) && isset($_POST['usersubject']) && isset($_POST['usermessage'])) {
     $from = "noreply@zosand.fr";
     $to = "webmaster@zosand.fr";
     $subject = $_POST['usersubject'];
-    $message = "Message from: " . $_POST['useremail']. " (" . $_POST['username'] . ") " . $_POST['usermessage'];
+    $message = "Message from: " . $_POST['useremail'] . " (" . $_POST['username'] . ") " . $_POST['usermessage'];
     $headers = "From:" . $from;
     mail($to, $subject, $message, $headers);
     $to = $_POST['useremail'];
-    $message = "You send a message to ". $to . ": " . $_POST['usermessage'];
+    $message = "You send a message to " . $to . ": " . $_POST['usermessage'];
     mail($to, $subject, $message, $headers);
 
 }
@@ -17,10 +16,10 @@ include("header.php");
 ?>
 
     <form action="#" method="post">
-        <input type="text" name="username" placeholder="Name" required><br>
-        <input type="text" name="useremail" placeholder="E-mail" required><br>
-        <input type="text" name="usersubject" placeholder="Subject" required><br>
-        <textarea name="usermessage" placeholder="Message" required ></textarea><br>
+        <input type="text" name="username" placeholder="Name" autocomplete="off" required><br>
+        <input type="email" name="useremail" placeholder="E-mail" autocomplete="off" required><br>
+        <input type="text" name="usersubject" placeholder="Subject" autocomplete="off" required><br>
+        <textarea name="usermessage" placeholder="Message" autocomplete="off" required></textarea><br>
         <input type="submit" value="Send">
     </form>
 
